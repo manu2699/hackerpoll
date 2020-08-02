c0d4514c04ecc1939705d616e6d795e2bc3c2e55
Maneesh <maneeshvijaykar@gmail.com>
2020-08-02 12:38:10 +0530

    url updates

--- client/src/utils/urls.js
+++ client/src/utils/urls.js
@@ -1,2 +1,2 @@
-export default "http://localhost:5000";
-// export default "https://hackerpoll.herokuapp.com";
+// export default "http://localhost:5000";
+export default "https://hackerpoll.herokuapp.com";
